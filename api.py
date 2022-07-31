7f41c221fd6c4879023634e48e0fd294ab31c166
Jan Holeček <90050233+jan-holecek@users.noreply.github.com>
2022-07-31 12:46:48 +0200

    Add files via upload

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ conn = http.client.HTTPSConnection("quotes15.p.rapidapi.com")
 
 headers = {
     'x-rapidapi-host': "quotes15.p.rapidapi.com",
-    'x-rapidapi-key': "KEY"
+    'x-rapidapi-key': "b18e61a8f8mshfed36e7f4426c8fp1ff287jsnc71baf639749"
 }
 
 conn.request("GET", "/quotes/random/?language_code=cs", headers=headers)
--- webhook.py
+++ webhook.py
@@ -1,18 +1,18 @@
 from discord_webhook import DiscordWebhook
-import content
+import main
 
 webhook = DiscordWebhook(
-    url="URL", 
+    url="https://discord.com/api/webhooks/1002879338036863096/rkoRJL12W39SYU6qcO8efiuzSUfO260PuYsHZrXKLVNbcesgANqwj7FmXq0qUuETvDFT", 
     username="Perfect_Citaty", 
-    content="Citát č." + str(content.number)
+    content="Citát č." + str(main.number)
 )
 #black
-with open("img/black/citat_cerny" + str(content.number) + ".png", "rb") as f:
-    webhook.add_file(file=f.read(), filename="citat_cerny" + str(content.number) + ".png")
+with open("img/black/citat_cerny" + str(main.number) + ".png", "rb") as f:
+    webhook.add_file(file=f.read(), filename="citat_cerny" + str(main.number) + ".png")
 
 #white
-with open("img/white/citat_bily" + str(content.number) + ".png", "rb") as f:
-    webhook.add_file(file=f.read(), filename="citat_bily" + str(content.number) + ".png")
+with open("img/white/citat_bily" + str(main.number) + ".png", "rb") as f:
+    webhook.add_file(file=f.read(), filename="citat_bily" + str(main.number) + ".png")
 
 response = webhook.execute()
-print(f"Citáty úspešně poslány na Discord server. (číslo: {str(content.number)})")  
+print(f"Citáty úspešně poslány na Discord server. (číslo: {str(main.number)})")  
