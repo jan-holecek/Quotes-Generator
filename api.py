eb7a17b3f29cda385e079efce5350c2484714ae0
Jan Holeček <90050233+jan-holecek@users.noreply.github.com>
2022-07-31 12:47:23 +0200

    Update api.py

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ conn = http.client.HTTPSConnection("quotes15.p.rapidapi.com")
 
 headers = {
     'x-rapidapi-host': "quotes15.p.rapidapi.com",
-    'x-rapidapi-key': "b18e61a8f8mshfed36e7f4426c8fp1ff287jsnc71baf639749"
+    'x-rapidapi-key': "KEY"
 }
 
 conn.request("GET", "/quotes/random/?language_code=cs", headers=headers)
